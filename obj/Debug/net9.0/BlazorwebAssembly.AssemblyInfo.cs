2235cf0e00ea9e0af39f20e51d5dc9b17645b4cf
Sushmitha Uppalapati <uppalapatisushmitha2003@gmail.com>
2025-11-10 17:51:08 +0530

    uichange

--- obj/Debug/net9.0/BlazorwebAssembly.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorwebAssembly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorwebAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa8747145ec314ed84fea583cc99295565b425d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorwebAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorwebAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
